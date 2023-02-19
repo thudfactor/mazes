192ae2a94ce2d6ed2e03068b0259e0e7a5ce19e4
John Williams <john@thudfactor.com>
2023-02-19 14:43:10 -0500

    Update meta with my mastodon link

--- src/components/meta.tsx
+++ src/components/meta.tsx
@@ -4,9 +4,13 @@ export function Meta() {
   return (
     <StyledMeta>
       <p>
-        Code adapted from <a href="http://www.mazesforprogrammers.com/">Mazes for Programmers</a> by Jamis Buck.{" "}
-        <a href="https://github.com/thudfactor/mazes">Source code on GitHub</a>. Special thanks to{" "}
-        <a href="https://tomyancey.tumblr.com/">Tom Yancey</a> for additional artwork.
+        I adapted much of the maze code from <a href="http://www.mazesforprogrammers.com/">Mazes for Programmers</a> by
+        Jamis Buck. <a href="https://github.com/thudfactor/mazes">Source code on GitHub</a>. Special thanks to{" "}
+        <a href="https://tomyancey.tumblr.com/">Tom Yancey</a> for additional artwork. Follow me on{" "}
+        <a rel="me" href="https://social.horrorhub.club/@thudfactor">
+          Mastodon
+        </a>
+        .
       </p>
     </StyledMeta>
   );
